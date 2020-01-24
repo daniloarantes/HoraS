2d1029731092669082b439f117680aee7e972c1a
daniloarantes <daniloarantes@gmail.com>
2020-01-24 16:08:23 -0300

    Update MainActivity.java
    Adicionado comentário pelo GitHub

--- app/src/main/java/com/daniloarantes/horas/MainActivity.java
+++ app/src/main/java/com/daniloarantes/horas/MainActivity.java
@@ -12,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Ao realizar modificações nos arquivos, é necessário realizar commit e push para o repositório do GitHub.
+        
+        // Adicionando comentário pelo GitHub. Para pegar novas atualizações, é necessário realizar o Push. Assim, as modificações serão 
+        // baixadas do github para seu computador, mantendo o projeto sempre atualizado e sincronizado.
+   
     }
 }
