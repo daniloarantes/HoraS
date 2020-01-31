1044269fc5f400abedfe5149c2a9f71d423b031a
daniloarantes <daniloarantes@gmail.com>
2020-01-31 13:18:07 -0300

    Teste AS

--- app/src/main/java/com/daniloarantes/horas/MainActivity.java
+++ app/src/main/java/com/daniloarantes/horas/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    //teste
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
